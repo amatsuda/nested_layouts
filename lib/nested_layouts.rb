ab034cb0040713cff368e09c40d6a7845ed1764e
Henrik N <henrik@nyh.se>
2008-10-24 19:50:18 +0200

    Whitespace.

--- lib/nested_layouts.rb
+++ lib/nested_layouts.rb
@@ -12,7 +12,7 @@ module ActionView #:nodoc:
         layout = layout.include?('/') ? layout : "layouts/#{layout}"
         @template.instance_variable_set('@content_for_layout', capture(&block))
         concat(
-          @template.render( :file => layout, :use_full_path => true )
+          @template.render(:file => layout, :use_full_path => true)
         )
       end
 
@@ -20,7 +20,7 @@ module ActionView #:nodoc:
       # Same as +inside_layout+ but takes layout template content rather than layout template name.
       def inside_inline_layout(template_content, &block)
         @template.instance_variable_set('@content_for_layout', capture(&block))
-        concat( @template.render( :inline => template_content ) )
+        concat(@template.render(:inline => template_content))
       end
     end
   end
